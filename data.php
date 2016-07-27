e1bcdc3dd15823051a7185b688622a508b7ed451
Admir Alicic <admir_alicic@yahoo.com>
2016-07-27 15:06:39 +0200

    ranges to int

--- data.php
+++ data.php
@@ -2,17 +2,17 @@
 
 function seedData($db){
     $db->exec('CREATE TABLE IF NOT EXISTS networks 
-        (network VARCHAR(20), range_start VARCHAR(20), range_end VARCHAR(20))');
+        (network VARCHAR(20), range_start INT, range_end INT)');
 
     $countResult = $db->query('SELECT COUNT(*) as rows FROM networks');
     $count = $countResult->fetchArray();
 
     if($count['rows'] < 3){
         $db->exec("INSERT INTO networks (network, range_start, range_end) VALUES 
-            ('10.0.0.0/8', '167772160', '184549375'),
-            ('10.1.0.0/16', '167837696', '167903231'),
-            ('127.0.0.0/8', '2130706432', '2147483647'),
-            ('192.168.8.0/24', '3232237568', '3232237823')");
+            ('10.0.0.0/8', 167772160, 184549375),
+            ('10.1.0.0/16', 167837696, 167903231),
+            ('127.0.0.0/8', 2130706432, 2147483647),
+            ('192.168.8.0/24', 3232237568, 3232237823)");
     };
 
     $countResult->finalize();
