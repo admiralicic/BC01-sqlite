3e92bfc84579d9a75bfe93de10cadff89ecf5329
Admir Alicic <admir_alicic@yahoo.com>
2016-07-27 13:27:26 +0200

    sql compare

--- data.php
+++ data.php
@@ -7,6 +7,7 @@ function seedData($db){
     $count = $countResult->fetchArray();
 
     if($count['rows'] < 3){
+        $db->exec("INSERT INTO networks (network) VALUES ('10.0.0.0/8')");
         $db->exec("INSERT INTO networks (network) VALUES ('10.1.0.0/16')");
         $db->exec("INSERT INTO networks (network) VALUES ('127.0.0.0/8')");
         $db->exec("INSERT INTO networks (network) VALUES ('192.168.8.0/24')");
@@ -15,8 +16,14 @@ function seedData($db){
     $countResult->finalize();
 }
 
-function getNetworks($db, $filter){
-    $results = $db->query('SELECT * FROM networks '.$filter);
+function getNetworks($db, $ip){
+
+    $ip_parts = explode(".", $ip);
+
+    $results = $db->query("SELECT * FROM networks WHERE 
+        network LIKE '$ip_parts[0]%/8' OR 
+        network LIKE '$ip_parts[0].$ip_parts[1]%/16' OR
+        network LIKE '$ip_parts[0].$ip_parts[1].$ip_parts[2]%/24'");
 
     $networks = [];
 
--- program.php
+++ program.php
@@ -16,31 +16,10 @@ if(!filter_var($argv[3], FILTER_VALIDATE_IP)){
 
 $db = new SQLite3($argv[2]);
 seedData($db);
-$networks = getNetworks($db, "WHERE network != '127.0.0.0/8'");
+$ip_address = $argv[3];
+$networks = getNetworks($db, $ip_address);
 $db->close();
 
-$ip_address = $argv[3];
-
-$convert_ip_to_int = function($ip){
-
-    $parts = explode(".", $ip);
-
-    return  (int)$parts[0] * (pow(2,24)-1) +
-            (int)$parts[1] * (pow(2,16)-1) +
-            (int)$parts[2] * (pow(2,8)-1) +
-            (int)$parts[3] * (pow(2,0));
-};
-
-$ip_address = $convert_ip_to_int($ip_address);
 foreach ($networks as $network) {
-    $parts = explode("/", $network);
-    $netaddress = $parts[0];
-    $subnet = $parts[1];
-
-    $rangeStart = $convert_ip_to_int($netaddress);
-    $rangeEnd = $rangeStart + (pow(2,32-(int)$subnet));
-
-    if($ip_address >= $rangeStart && $ip_address <= $rangeEnd){
         echo $network.PHP_EOL;
-    }
 }
